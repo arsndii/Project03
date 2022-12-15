14f1f2e48683d90459dfdd7ded83548715644018
Arisandi <arisandii033@gmail.com>
2022-12-15 23:37:11 +0800

    Buat method cetakInfo

--- src/project03/Sepeda.java
+++ src/project03/Sepeda.java
@@ -9,7 +9,11 @@ package project03;
  * @author warik
  */
 public class Sepeda {
-    String nerk;
+    String merk;
     String warna;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t: " + merk + "\n" + "Warna \t: " + warna + "\n" + "Harga \t: " + harga);
+    }
 }
